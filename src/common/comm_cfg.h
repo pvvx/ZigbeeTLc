0240e7bcbbc404b7386d352886a79da556124bb9
pvvx <pvvx@mail.ru>
2023-11-16 21:27:15 +0300

    Assigning firmware numbers

--- src/common/comm_cfg.h
+++ src/common/comm_cfg.h
@@ -84,22 +84,26 @@ enum {
 */
 
 /* Board ID */
-#define BOARD_LYWSD03MMC_B14	0
-#define BOARD_MHO_C401			1
-#define BOARD_CGG1				2
-//#define BOARD_LYWSD03MMC		3 // ver devbis
-#define BOARD_LYWSD03MMC_B19	3
-#define BOARD_LYWSD03MMC_B16	4
-#define BOARD_LYWSD03MMC_B17	5
-#define BOARD_CGDK2				6
-#define BOARD_CGG1N				7 // 2022
-#define BOARD_MHO_C401N			8 // 2022
-#define BOARD_MJWSD05MMC		9
-#define BOARD_LYWSD03MMC		10
-#define BOARD_LYWSD03MMC_B15	10
-#define BOARD_MHO_C122			11
-
-#define BOARD_TS0201_TZ3000		17
+//#define BOARD_LYWSD03MMC_B14		0 // number used for BLE firmware
+#define BOARD_MHO_C401				1
+#define BOARD_CGG1					2
+//#define BOARD_LYWSD03MMC_B19		3 // number used for BLE firmware
+//#define BOARD_LYWSD03MMC_DEVBIS	3 // ver https://github.com/devbis/z03mmc
+//#define BOARD_LYWSD03MMC_B16		4 // number used for BLE firmware
+//#define BOARD_WATERMETER			4 // ver https://github.com/slacky1965/watermeter_zed 
+//#define BOARD_LYWSD03MMC_B17		5 // number used for BLE firmware
+#define BOARD_CGDK2					6
+#define BOARD_CGG1N					7 // 2022
+#define BOARD_MHO_C401N				8 // 2022
+#define BOARD_MJWSD05MMC			9
+//#define BOARD_LYWSD03MMC_B15		10 // number used for BLE firmware
+#define BOARD_LYWSD03MMC			10
+#define BOARD_MHO_C122				11
+// 13..15 - number used for BLE firmware - https://github.com/pvvx/ATC_MiThermometer
+#define BOARD_TNK					16 // Water tank controller (not yet published at the moment)
+#define BOARD_TS0201_TZ3000			17
+#define BOARD_TS0202_TZ3000			18 // ?
+// 19.. not yet appointed
 
 /* Board define */
 #ifndef BOARD
