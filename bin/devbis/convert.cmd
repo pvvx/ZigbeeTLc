e872164c840dc79cba9ad3efe16535952a34a0c5
pvvx <pvvx@mail.ru>
2024-02-13 05:08:59 +0300

    convert ver devbis <-> ZigbeeTLc

---
+++ bin/devbis/convert.cmd
@@ -0,0 +1,4 @@
+python3 ..\..\make\zb_bin_ota.py z03mmc.bin z03mmc_v1100 -m0x1141 -i0x020a -v0x09993001 -s"Zigbee ver: ZigbeeTLc to devis"
+python3 ..\..\make\zb_bin_ota.py ..\Z03MMC_v0120.bin Z03MMC_v0120 -m0x1141 -i0x0203 -v0x79993001 -s"Zigbee ver: devis to ZigbeeTLc"
+python3 ..\..\make\zb_bin_ota.py ..\Z03MMC_v0120.bin Z03MMC_v0120 -m0xDB15 -i0x0203 -v0x79993001 -s"Zigbee ver: devis to ZigbeeTLc"
+
---
+++ bin/devbis/readme.txt
@@ -0,0 +1,4 @@
+z03mmc.bin - copy from https://github.com/devbis/z03mmc/releases/download/1.1.0/z03mmc.bin
+1141-020a-09993001-z03mmc_v1100.zigbee - Zigbee OTA ver: ZigbeeTLc to devis v1.1.0.0
+1141-0203-09993001-Z03MMC_v0120.zigbee - Zigbee OTA ver: devis old ver1.0.6 to ZigbeeTLc
+db15-0203-09993001-Z03MMC_v0120.zigbee - Zigbee OTA ver: devis new ver1.1.0 to ZigbeeTLc
