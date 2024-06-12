502bc422f5a9cbfbbac4ec6ab3c4aa292987d9c7
Victor <pvvx@mail.ru>
2024-06-12 11:40:47 +0300

    add ZTH0x

--- z2m/zigbeetlc_v0121.js
+++ z2m/zigbeetlc_v0121.js
@@ -221,7 +221,9 @@ const definitions = [
             {modelID: 'TS0201-z', manufacturerName: 'Tuya', model: 'TS0201'},
             {modelID: 'TS0201-bz', manufacturerName: 'Tuya', model: 'TS0201'},
             {modelID: 'TH03Z-z', manufacturerName: 'Tuya', model: 'TH03Z'},
-            {modelID: 'TH03Z-bz', manufacturerName: 'Tuya', model: 'TH03Z'}
+            {modelID: 'TH03Z-bz', manufacturerName: 'Tuya', model: 'TH03Z'},
+			{modelID: 'ZTH01-z', manufacturerName: 'Tuya', model: 'ZTH01'},
+			{modelID: 'ZTH02-z', manufacturerName: 'Tuya', model: 'ZTH02'}
         ],
         model: 'WSD500A',
         vendor: 'Tuya',
