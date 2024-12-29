b95135d31fc3272f5921330e82cd346d5c300959
pvvx <pvvx@mail.ru>
2024-12-29 18:04:50 +0300

    update v0.1.2.3

--- bin/devbis/convert.cmd
+++ bin/devbis/convert.cmd
@@ -1,5 +1,5 @@
 python3 ..\..\make\zigbee_ota.py z03mmc.bin
 python3 ..\..\make\zb_bin_ota.py z03mmc.bin z03mmc_v1100 -m0x1141 -i0x020a -v0x09993001 -s"Zigbee ver: ZigbeeTLc to devbis"
-python3 ..\..\make\zb_bin_ota.py ..\Z03MMC_v0122.bin Z03MMC_v0122 -m0x1141 -i0x0203 -v0x79993001 -s"Zigbee ver: devbis to ZigbeeTLc"
-python3 ..\..\make\zb_bin_ota.py ..\Z03MMC_v0122.bin Z03MMC_v0122 -m0xDB15 -i0x0203 -v0x79993001 -s"Zigbee ver: devbis to ZigbeeTLc"
+python3 ..\..\make\zb_bin_ota.py ..\Z03MMC_v0123.bin Z03MMC_v0123 -m0x1141 -i0x0203 -v0x79993001 -s"Zigbee ver: devbis to ZigbeeTLc"
+python3 ..\..\make\zb_bin_ota.py ..\Z03MMC_v0123.bin Z03MMC_v0123 -m0xDB15 -i0x0203 -v0x79993001 -s"Zigbee ver: devbis to ZigbeeTLc"
 
